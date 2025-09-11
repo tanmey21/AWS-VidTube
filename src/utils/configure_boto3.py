d705817ae4c330f4ac0340c0e98dcc6bdb9765a2
Tanmey Agarwal <tanmayjayjawan@gmail.com>
2025-09-12 00:26:41 +0530

    Transferred my secrets to .env file

--- src/utils/configure_boto3.py
+++ src/utils/configure_boto3.py
@@ -1,9 +1,13 @@
 import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def client_connect(service_name):
     client = boto3.client(service_name,
-                          region_name='us-east-1',
-                          aws_access_key_id='AKIAW4QRUXAW3BTBVTUM',
-                          aws_secret_access_key='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',
+                          region_name=os.getenv('REGION_NAME'),
+                          aws_access_key_id=os.getenv('ACCESS_KEY'),
+                          aws_secret_access_key=os.getenv('SECRET_KEY'),
                          )
     return client
